2c70b8c048258d92b09caffd99b3509fbff0d1b5
Julia <juliabrain2020@u.northwestern.edu>
2020-03-11 12:36:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./App.scss"
-import { AuthButton } from "components/AuthButton"
+import { AuthButton } from "./components/AuthButton"
 
 function App() {
   return (
