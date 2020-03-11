d9fe63182c529de5a91b5743356f0c27e96eeaa4
Julia <juliabrain2020@u.northwestern.edu>
2020-03-11 12:31:12 -0500

    fixed components/loading issue

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react"
 import { Redirect, withRouter } from "react-router-dom"
 import { UserContext } from "./context/UserContext/UserContext"
-import { Loading } from "components/Loading"
+import { Loading } from "./components/Loading"
 
 const ProtectedRoute = ({ component, children, ...rest }) => {
   const { isLoading, isAuthorized } = useContext(UserContext)
